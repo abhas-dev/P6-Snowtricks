8553ee13c21d48103b214d489d4e178a4e696674
Restyled.io <commits@restyled.io>
2021-11-19 18:00:30 +0000

    Restyled by prettier

--- app/assets/js/formImage.js
+++ app/assets/js/formImage.js
@@ -1,12 +1,15 @@
 const addFormToCollection = (e) => {
   const collectionHolder = document.querySelector(
-      '.' + e.currentTarget.dataset.collectionHolderClass);
+    "." + e.currentTarget.dataset.collectionHolderClass
+  );
 
-  const item = document.createElement('li');
+  const item = document.createElement("li");
   item.className = "my-2";
 
   item.innerHTML = collectionHolder.dataset.prototype.replace(
-      /__name__/g, collectionHolder.dataset.index);
+    /__name__/g,
+    collectionHolder.dataset.index
+  );
 
   collectionHolder.appendChild(item);
 
@@ -15,19 +18,18 @@ const addFormToCollection = (e) => {
   addImageFormDeleteLink(item);
 };
 
-const addImageFormDeleteLink =
-    (tagFormLi) => {
-      const removeFormButton = document.createElement('button')
-      removeFormButton.classList = "btn btn-danger"
-      removeFormButton.innerText = 'Supprimer cette image'
-      tagFormLi.append(removeFormButton);
+const addImageFormDeleteLink = (tagFormLi) => {
+  const removeFormButton = document.createElement("button");
+  removeFormButton.classList = "btn btn-danger";
+  removeFormButton.innerText = "Supprimer cette image";
+  tagFormLi.append(removeFormButton);
 
-      removeFormButton.addEventListener('click', (e) => {
-        e.preventDefault()
-        tagFormLi.remove();
-      });
-    }
+  removeFormButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    tagFormLi.remove();
+  });
+};
 
-                   document.querySelectorAll('.add_item_link')
-                       .forEach(btn => btn.addEventListener(
-                                    "click", addFormToCollection));
+document
+  .querySelectorAll(".add_item_link")
+  .forEach((btn) => btn.addEventListener("click", addFormToCollection));
