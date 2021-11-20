204225a8143076740e479127960683a0eacf027e
Restyled.io <commits@restyled.io>
2021-11-20 14:52:32 +0000

    Restyled by prettier

--- app/assets/js/editTrick.js
+++ app/assets/js/editTrick.js
@@ -2,32 +2,37 @@ const axios = require("axios");
 
 // Change div content
 function showOutput(res) {
-  document.querySelector('#js-content').innerHTML = ``
+  document.querySelector("#js-content").innerHTML = ``;
 }
 
-let links = document.querySelectorAll('[data-delete]');
+let links = document.querySelectorAll("[data-delete]");
 // Delete Image
 function onClickDeleteBtn(e) {
   e.preventDefault();
   const url = this.href;
-  if (confirm('Voulez-vous vraiment supprimer cette image?')) {
+  if (confirm("Voulez-vous vraiment supprimer cette image?")) {
     axios
-        .delete(this.getAttribute('href'), {
-          headers : {'X-Requested-With' : 'XMLHttpRequest'},
-          // On recupere le token dans le dataset l'attribut token
-          data : {"_token" : this.dataset.token}
-        })
-        .then(response => response.data)
-        .then(data => {data.code === 200 ? this.parentElement.remove()
-                                         : alert(data.message)})
-        .catch(error => { console.log(error); })
+      .delete(this.getAttribute("href"), {
+        headers: { "X-Requested-With": "XMLHttpRequest" },
+        // On recupere le token dans le dataset l'attribut token
+        data: { _token: this.dataset.token },
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        data.code === 200 ? this.parentElement.remove() : alert(data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
 
-links.forEach(link => {link.addEventListener('click', onClickDeleteBtn)});
+links.forEach((link) => {
+  link.addEventListener("click", onClickDeleteBtn);
+});
 
 // Set Main Picture
-const radios = document.querySelectorAll('.js-mainPicture');
+const radios = document.querySelectorAll(".js-mainPicture");
 
 async function onSelectMainPicture(e) {
   e.preventDefault();
@@ -38,4 +43,6 @@ async function onSelectMainPicture(e) {
   }
 }
 
-radios.forEach(radio => radio.addEventListener('change', onSelectMainPicture));
+radios.forEach((radio) =>
+  radio.addEventListener("change", onSelectMainPicture)
+);
--- app/assets/js/removeTrick.js
+++ app/assets/js/removeTrick.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 
-const removeTrickBtn = document.querySelectorAll('#js-deleteTrick');
-const trickCard = document.querySelectorAll('#js-trickCard');
+const removeTrickBtn = document.querySelectorAll("#js-deleteTrick");
+const trickCard = document.querySelectorAll("#js-trickCard");
 
 async function onRemoveTrick(e) {
   e.preventDefault();
   try {
-    const response =
-        await axios
-            .delete(this.getAttribute('href'), {
-              headers : {'X-Requested-With' : 'XMLHttpRequest'},
-              // On recupere le token dans le dataset l'attribut token
-              data : {"_token" : this.dataset.token}
-            })
-            .then(response => response.data)
-            .then(data => {data.code === 200
-                               ? this.parentElement.parentElement.parentElement
-                                     .parentElement.remove()
-                               : alert(data.message)})
-
+    const response = await axios
+      .delete(this.getAttribute("href"), {
+        headers: { "X-Requested-With": "XMLHttpRequest" },
+        // On recupere le token dans le dataset l'attribut token
+        data: { _token: this.dataset.token },
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        data.code === 200
+          ? this.parentElement.parentElement.parentElement.parentElement.remove()
+          : alert(data.message);
+      });
   } catch (e) {
     console.log(e);
   }
 }
 
-removeTrickBtn.forEach(
-    removeBtn => {removeBtn.addEventListener('click', onRemoveTrick)});
+removeTrickBtn.forEach((removeBtn) => {
+  removeBtn.addEventListener("click", onRemoveTrick);
+});
