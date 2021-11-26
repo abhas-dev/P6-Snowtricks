81c5e776069a38036ed823e8879306c9232b4482
Restyled.io <commits@restyled.io>
2021-11-26 07:56:39 +0000

    Restyled by clang-format

--- app/assets/js/editTrick.js
+++ app/assets/js/editTrick.js
@@ -1,51 +1,50 @@
 const axios = require("axios");
 
 // Change div content
-function showOutput(res){
-    document.querySelector('#js-content').innerHTML = ``
+function showOutput(res) {
+  document.querySelector('#js-content').innerHTML = ``
 }
 
 let links = document.querySelectorAll('[data-delete]');
 // Delete Image
 function onClickDeleteBtn(e) {
-    e.preventDefault();
-    // const url = this.href;
-    const url = this.getAttribute('href');
-    let id = url.match( /image-delete\/(.+)/)[1];
+  e.preventDefault();
+  // const url = this.href;
+  const url = this.getAttribute('href');
+  let id = url.match(/image-delete\/(.+)/)[1];
 
-    if (confirm('Voulez-vous vraiment supprimer cette image?')) {
-        axios.delete(this.getAttribute('href'), {
-            headers: {'X-Requested-With': 'XMLHttpRequest'},
-            // On recupere le token dans le dataset l'attribut token
-            data: {"_token": this.dataset.token}
+  if (confirm('Voulez-vous vraiment supprimer cette image?')) {
+    axios
+        .delete(this.getAttribute('href'), {
+          headers : {'X-Requested-With' : 'XMLHttpRequest'},
+          // On recupere le token dans le dataset l'attribut token
+          data : {"_token" : this.dataset.token}
         })
-            .then(response => response.data)
-            .then(data => {
-                let trickImageDiv = document.getElementById("trickImage-" + id);
-                data.code === 200 ? trickImageDiv.parentNode.removeChild(trickImageDiv) : alert(data.message);
-                // data.code === 200 ? this.parentElement.parentElement.remove() : alert(data.message)
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    }
+        .then(response => response.data)
+        .then(data => {
+          let trickImageDiv = document.getElementById("trickImage-" + id);
+          data.code === 200
+              ? trickImageDiv.parentNode.removeChild(trickImageDiv)
+              : alert(data.message);
+          // data.code === 200 ? this.parentElement.parentElement.remove() :
+          // alert(data.message)
+        })
+        .catch(error => { console.log(error); })
+  }
 }
 
-links.forEach(link => {
-    link.addEventListener('click', onClickDeleteBtn)
-});
-
+links.forEach(link => {link.addEventListener('click', onClickDeleteBtn)});
 
 // Set Main Picture
 const radios = document.querySelectorAll('.js-mainPicture');
 
-async function onSelectMainPicture(e){
-    e.preventDefault();
-    try{
-        const response = await axios.post(this.dataset.mainpicture);
-    } catch(error){
-        console.log(error);
-    }
+async function onSelectMainPicture(e) {
+  e.preventDefault();
+  try {
+    const response = await axios.post(this.dataset.mainpicture);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 radios.forEach(radio => radio.addEventListener('change', onSelectMainPicture));
--- app/assets/js/removeTrick.js
+++ app/assets/js/removeTrick.js
@@ -2,29 +2,31 @@ import axios from "axios";
 
 const removeTrickBtn = document.querySelectorAll('#js-deleteTrick');
 
-async function onRemoveTrick(e){
-    e.preventDefault();
-    try {
-        const url = this.getAttribute('href');
-        let slug = url.match( /trick\/(.+)\/delete/)[1];
+async function onRemoveTrick(e) {
+  e.preventDefault();
+  try {
+    const url = this.getAttribute('href');
+    let slug = url.match(/trick\/(.+)\/delete/)[1];
 
-        const response = await axios.delete(url, {
-            headers: {'X-Requested-With': 'XMLHttpRequest'},
-            // On recupere le token dans le dataset l'attribut token
-            data: {"_token": this.dataset.token}
-        })
+    const response =
+        await axios
+            .delete(url, {
+              headers : {'X-Requested-With' : 'XMLHttpRequest'},
+              // On recupere le token dans le dataset l'attribut token
+              data : {"_token" : this.dataset.token}
+            })
             .then(response => response.data)
             .then(data => {
-                let trickDiv = document.getElementById("trick-" + slug);
-                data.code === 200 ? trickDiv.parentNode.removeChild(trickDiv) : alert(data.message)
-                // this.parentElement.parentElement.parentElement.parentElement.remove()
+              let trickDiv = document.getElementById("trick-" + slug);
+              data.code === 200 ? trickDiv.parentNode.removeChild(trickDiv)
+                                : alert(data.message)
+              // this.parentElement.parentElement.parentElement.parentElement.remove()
             })
 
-    } catch (e) {
-        console.log(e);
-    }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-removeTrickBtn.forEach(removeBtn => {
-    removeBtn.addEventListener('click', onRemoveTrick)
-});
+removeTrickBtn.forEach(
+    removeBtn => {removeBtn.addEventListener('click', onRemoveTrick)});
