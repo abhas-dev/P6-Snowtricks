06dbe69cd32a0d3a88cb4d0b5d875d1233c99fab
Restyled.io <commits@restyled.io>
2021-12-09 12:38:18 +0000

    Restyled by prettier

--- app/assets/js/homepage_loadmore.js
+++ app/assets/js/homepage_loadmore.js
@@ -1,32 +1,34 @@
 const axios = require("axios");
 
-const loadMoreBtn = document.querySelector('#js-loadMore_tricks');
-const tricksBlock = document.querySelector('#tricks');
-const loadMoreSection = document.querySelector('#loadMoreSection');
+const loadMoreBtn = document.querySelector("#js-loadMore_tricks");
+const tricksBlock = document.querySelector("#tricks");
+const loadMoreSection = document.querySelector("#loadMoreSection");
 let nextPage = 2;
 
 async function getMoreTricks(e) {
   e.preventDefault();
-  const url = this.getAttribute('href');
-  loadMoreBtn.textContent = 'Chargement...';
+  const url = this.getAttribute("href");
+  loadMoreBtn.textContent = "Chargement...";
   await axios({
-    method : 'post',
-    url : `${url}?page=${nextPage}`,
-    headers : {
-      "X-Requested-With" : "XMLHttpRequest",
-      "Content-Type" : "application/json"
+    method: "post",
+    url: `${url}?page=${nextPage}`,
+    headers: {
+      "X-Requested-With": "XMLHttpRequest",
+      "Content-Type": "application/json",
     },
-    data : {lastTrick : this.dataset.lastTrick}
+    data: { lastTrick: this.dataset.lastTrick },
   })
-      .then(response => response.data)
-      .then(data => {
-        tricksBlock.insertAdjacentHTML('beforeend', data._template.content);
-        nextPage = data.nextPage;
-        !data.nextPage
-            ? loadMoreSection.innerHTML = "Il n'y a pas d'autre trick"
-            : loadMoreBtn.textContent = 'Charger Plus';
-      })
-      .catch(e => { console.log(e); });
+    .then((response) => response.data)
+    .then((data) => {
+      tricksBlock.insertAdjacentHTML("beforeend", data._template.content);
+      nextPage = data.nextPage;
+      !data.nextPage
+        ? (loadMoreSection.innerHTML = "Il n'y a pas d'autre trick")
+        : (loadMoreBtn.textContent = "Charger Plus");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
 
-loadMoreBtn.addEventListener('click', getMoreTricks);
+loadMoreBtn.addEventListener("click", getMoreTricks);
