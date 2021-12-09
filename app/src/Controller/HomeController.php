b6e10783fc6a7c4761e65134357f106feb99764c
Abdelatif Hassaine <ab.hassaine@protonmail.com>
2021-12-09 16:41:39 +0100

    load more implemented

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -3,16 +3,50 @@
 namespace App\Controller;
 
 use App\Repository\TrickRepository;
+use App\Service\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(TrickRepository $trickRepository): Response
+    private TrickRepository $trickRepository;
+    private RequestStack $requestStack;
+    private Paginator $paginator;
+
+    public function __construct(TrickRepository $trickRepository, RequestStack $requestStack, Paginator $paginator)
     {
-        $tricks = $trickRepository->findAll();
+        $this->trickRepository = $trickRepository;
+        $this->requestStack = $requestStack;
+        $this->paginator = $paginator;
+    }
+
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        $request = $this->requestStack->getCurrentRequest();
+
+        // check whether request is called directly via route or via Twig template
+        $isMainRequest = $this->requestStack->getMainRequest() === $request;
+
+        $tricks = $this->trickRepository->findBy([], ['createdAt' => 'ASC'], 10, 0);
+
+        if ($request->isXmlHttpRequest() || !$isMainRequest) {
+            $data = json_decode($request->getContent(), true);
+
+            $paginatedTricks = $this->paginator->loadMoreTricks($currentPage = $request->query->get('page'));
+            [
+                $tricks,
+                $totalPages,
+                $currentPage
+            ] = $paginatedTricks;
+
+            return $this->json([
+                '_template' => $this->render('trick/_list.html.twig', compact('tricks', 'totalPages', 'currentPage')),
+                'nextPage' => $currentPage + 1 <= $totalPages ? $currentPage + 1 : false
+            ], Response::HTTP_OK);
+        }
 
         return $this->render('home/index.html.twig', compact('tricks'));
     }
