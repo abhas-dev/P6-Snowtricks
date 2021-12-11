60e19189e7d2903119fcf727c6178bef0c9422c6
Restyled.io <commits@restyled.io>
2021-12-11 18:32:56 +0000

    Restyled by prettier

--- app/assets/js/editTrick.js
+++ app/assets/js/editTrick.js
@@ -11,25 +11,29 @@ function onClickDeleteBtn(e) {
 
   if (confirm("Voulez-vous vraiment supprimer cette image?")) {
     axios
-        .delete(this.getAttribute("href"), {
-          headers : {"X-Requested-With" : "XMLHttpRequest"},
-          // On recupere le token dans le dataset l'attribut token
-          data : {_token : this.dataset.token},
-        })
-        .then((response) => response.data)
-        .then((data) => {
-          let trickImageDiv = document.getElementById("trickImage-" + id);
-          data.code === 200
-              ? trickImageDiv.parentNode.removeChild(trickImageDiv)
-              : alert(data.message);
-          // data.code === 200 ? this.parentElement.parentElement.remove() :
-          // alert(data.message)
-        })
-        .catch((error) => { console.log(error); });
+      .delete(this.getAttribute("href"), {
+        headers: { "X-Requested-With": "XMLHttpRequest" },
+        // On recupere le token dans le dataset l'attribut token
+        data: { _token: this.dataset.token },
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        let trickImageDiv = document.getElementById("trickImage-" + id);
+        data.code === 200
+          ? trickImageDiv.parentNode.removeChild(trickImageDiv)
+          : alert(data.message);
+        // data.code === 200 ? this.parentElement.parentElement.remove() :
+        // alert(data.message)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
 
-links.forEach((link) => { link.addEventListener("click", onClickDeleteBtn); });
+links.forEach((link) => {
+  link.addEventListener("click", onClickDeleteBtn);
+});
 
 /********************Set Main Picture********************/
 const radios = document.querySelectorAll(".js-mainPicture");
@@ -44,33 +48,34 @@ async function onSelectMainPicture(e) {
 }
 
 radios.forEach((radio) =>
-                   radio.addEventListener("change", onSelectMainPicture));
+  radio.addEventListener("change", onSelectMainPicture)
+);
 
 /********************Change div content********************/
-const tabs = document.querySelectorAll('.js-trickEditTab');
+const tabs = document.querySelectorAll(".js-trickEditTab");
 
 async function showEdit(e) {
   e.preventDefault();
   try {
     fetch(this.href, {
-      method : "POST",
-      body : this.dataset.content,
-      headers : {'X-Requested-With' : 'XMLHttpRequest'}
+      method: "POST",
+      body: this.dataset.content,
+      headers: { "X-Requested-With": "XMLHttpRequest" },
     })
-        .then(response => response.json())
-        .then(data => {
-          document.querySelector(".js-content").innerHTML =
-              data._template.content;
-          let deleteVideoLinks = document.querySelectorAll("[data-delete]");
-          deleteVideoLinks.forEach(
-              link => link.addEventListener('click', onClickDeleteVideo));
-        });
-
+      .then((response) => response.json())
+      .then((data) => {
+        document.querySelector(".js-content").innerHTML =
+          data._template.content;
+        let deleteVideoLinks = document.querySelectorAll("[data-delete]");
+        deleteVideoLinks.forEach((link) =>
+          link.addEventListener("click", onClickDeleteVideo)
+        );
+      });
   } catch (error) {
     console.log(error);
   }
 }
-tabs.forEach(tab => tab.addEventListener('click', showEdit));
+tabs.forEach((tab) => tab.addEventListener("click", showEdit));
 
 /********************Delete Video********************/
 
@@ -80,19 +85,22 @@ async function onClickDeleteVideo(e) {
 
   if (confirm("Voulez-vous vraiment supprimer cette video?")) {
     axios
-        .delete(url, {
-          headers : {"X-Requested-With" : "XMLHttpRequest"},
-          data : {_token : this.dataset.token},
-        })
-        .then((response) => response.data)
-        .then((data) => {
-          let trickVideoDiv =
-              document.querySelector(".trickVideo-" + data.trickVideoId);
-          data.code === 200
-              // ? console.log(trickVideoDiv)
-              ? trickVideoDiv.remove()
-              : alert(data.message);
-        })
-        .catch((error) => { console.log(error); });
+      .delete(url, {
+        headers: { "X-Requested-With": "XMLHttpRequest" },
+        data: { _token: this.dataset.token },
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        let trickVideoDiv = document.querySelector(
+          ".trickVideo-" + data.trickVideoId
+        );
+        data.code === 200
+          ? // ? console.log(trickVideoDiv)
+            trickVideoDiv.remove()
+          : alert(data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
--- app/assets/js/homepage.js
+++ app/assets/js/homepage.js
@@ -1,40 +1,39 @@
 const axios = require("axios");
 
-const loadMoreBtn = document.querySelector('#js-loadMore_tricks');
-const tricksBlock = document.querySelector('#tricks');
-const loadMoreSection = document.querySelector('#loadMoreSection');
+const loadMoreBtn = document.querySelector("#js-loadMore_tricks");
+const tricksBlock = document.querySelector("#tricks");
+const loadMoreSection = document.querySelector("#loadMoreSection");
 let nextPage = 2;
 
 async function onRemoveTrick(e) {
   e.preventDefault();
   try {
-    const url = this.getAttribute('href');
+    const url = this.getAttribute("href");
     let slug = url.match(/trick\/(.+)\/delete/)[1];
 
-    const response =
-        await axios
-            .delete(url, {
-              headers : {'X-Requested-With' : 'XMLHttpRequest'},
-              data : {"_token" : this.dataset.token}
-            })
-            .then(response => response.data)
-            .then(data => {
-              let trickDiv = document.getElementById("trick-" + slug);
-              data.code === 200 ? trickDiv.parentNode.removeChild(trickDiv)
-                                : alert(data.message)
-            })
-
+    const response = await axios
+      .delete(url, {
+        headers: { "X-Requested-With": "XMLHttpRequest" },
+        data: { _token: this.dataset.token },
+      })
+      .then((response) => response.data)
+      .then((data) => {
+        let trickDiv = document.getElementById("trick-" + slug);
+        data.code === 200
+          ? trickDiv.parentNode.removeChild(trickDiv)
+          : alert(data.message);
+      });
   } catch (e) {
     console.log(e);
   }
 }
 
 function bindRemoveTrick() {
-  let removeTrickBtn = document.querySelectorAll('.js-deleteTrick');
+  let removeTrickBtn = document.querySelectorAll(".js-deleteTrick");
 
-  removeTrickBtn.forEach(removeBtn => {
-    removeBtn.removeEventListener('click', onRemoveTrick);
-    removeBtn.addEventListener('click', onRemoveTrick);
+  removeTrickBtn.forEach((removeBtn) => {
+    removeBtn.removeEventListener("click", onRemoveTrick);
+    removeBtn.addEventListener("click", onRemoveTrick);
   });
 }
 
@@ -42,27 +41,29 @@ bindRemoveTrick();
 
 async function getMoreTricks(e) {
   e.preventDefault();
-  const url = this.getAttribute('href');
-  loadMoreBtn.textContent = 'Chargement...';
+  const url = this.getAttribute("href");
+  loadMoreBtn.textContent = "Chargement...";
   await axios({
-    method : 'post',
-    url : `${url}?page=${nextPage}`,
-    headers : {
-      "X-Requested-With" : "XMLHttpRequest",
-      "Content-Type" : "application/json"
+    method: "post",
+    url: `${url}?page=${nextPage}`,
+    headers: {
+      "X-Requested-With": "XMLHttpRequest",
+      "Content-Type": "application/json",
     },
-    data : {lastTrick : this.dataset.lastTrick}
+    data: { lastTrick: this.dataset.lastTrick },
   })
-      .then(response => response.data)
-      .then(data => {
-        tricksBlock.insertAdjacentHTML('beforeend', data._template.content);
-        nextPage = data.nextPage;
-        !data.nextPage
-            ? loadMoreSection.innerHTML = "Il n'y a pas d'autre trick"
-            : loadMoreBtn.textContent = 'Charger Plus';
-        bindRemoveTrick();
-      })
-      .catch(e => { console.log(e); });
+    .then((response) => response.data)
+    .then((data) => {
+      tricksBlock.insertAdjacentHTML("beforeend", data._template.content);
+      nextPage = data.nextPage;
+      !data.nextPage
+        ? (loadMoreSection.innerHTML = "Il n'y a pas d'autre trick")
+        : (loadMoreBtn.textContent = "Charger Plus");
+      bindRemoveTrick();
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
 
-loadMoreBtn.addEventListener('click', getMoreTricks);
+loadMoreBtn.addEventListener("click", getMoreTricks);
